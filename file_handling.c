4539bc2e60ad58fb3a5b3a613843c2566a18c4e7
Andrew McKenzie <amcken33@myune.edu.au>
2024-07-13 21:08:08 +1000

    Add commenting

--- file_handling.c
+++ file_handling.c
@@ -8,7 +8,14 @@
 
 #define MAX_FILENAME 256
 
-int calc_num_files(const char* directory) {
+/**
+ * get_num_files() - Calculates the number of files in given directory
+ * @arg1: pathname of directory to search inside
+ *
+ * Return: Integer number representing the total number of files
+ */
+int get_num_files(const char* directory)
+{
         DIR* dp;
         struct dirent* dirp;
         int count = 0;
@@ -26,7 +33,19 @@ int calc_num_files(const char* directory) {
         return count;
 }
 
-void get_file_names(char** filesArray, const char* directory){
+/**
+ * get_file_names() - Extracts filenames from inside directory
+ * @arg1: Pointer to start of char array for holding the names
+ * @arg2: Pointer to the directory pathname to get the filenames from
+ *
+ *  Function goes through the given directory, any file that is not hidden
+ *  or the special '.' and '..' files will have its name recorded into the
+ *  given array.
+ *
+ * Return: void (pass by reference)
+ */
+void get_file_names(char** filesArray, const char* directory)
+{
         DIR* dp;
         struct dirent* dirp;
         dp = opendir(directory);
@@ -34,8 +53,7 @@ void get_file_names(char** filesArray, const char* directory){
 
         while((dirp = readdir(dp)) != NULL) {
                 if (dirp->d_type == DT_REG && dirp->d_name[0] != '.') {
-                        filesArray[index] = (char*)malloc((strlen
-                                                                   (dirp->d_name) + 1) * sizeof(char));
+                        filesArray[index] = (char*)malloc((strlen(dirp->d_name) + 1) * sizeof(char));
                         strcpy(filesArray[index], dirp->d_name);
                         index++;
                 }
@@ -44,7 +62,22 @@ void get_file_names(char** filesArray, const char* directory){
         closedir(dp);
 }
 
-long calc_file_counts(char inFile[], long char_stats[], const char* directory) {
+/**
+ * calc_file_counts() - Counts frequency of each char in given file
+ * @arg1: File name to access
+ * @arg2: Array for the numeric count of each character
+ * @arg3: Directory the file is inside
+ *
+ * Function accesses given file, reads the file while checking if each
+ * character is one of the 26 between A and Z and increments that count
+ * in the given char_stats array.
+ *
+ * Reference: Function slightly modified from assignment description
+ *
+ * Return: The array of numeric count of each character
+ */
+long calc_file_counts(char inFile[], long char_stats[], const char* directory)
+{
         char cur_fname[MAX_FILENAME] = "";
         strcpy(cur_fname, directory);
         strcat(cur_fname, "/");
--- file_handling.h
+++ file_handling.h
@@ -1,7 +1,7 @@
 #ifndef A1_FILES_H
 #define A1_FILES_H
 
-int calc_num_files(const char* directory);
+int get_num_files(const char* directory);
 void get_file_names(char** filesArray, const char* directory);
 long calc_file_counts(char inFile[], long char_stats[], const char* directory);
 
